51b7ae93a683368cb7f2dd9a280abcd53d14fb68
Kevin Park <keunsuk413@gmail.com>
2020-04-14 21:47:35 -0500

    added delete button in note page

--- src/NotePage.js
+++ src/NotePage.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import NotefulContext from './NotefulContext'
 import './NotePage.css';
 
+function deleteNoteRequest(noteId, cb) {
+  fetch(`https://ancient-river-87952.herokuapp.com/api/notes/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        // get the error message from the response,
+        return res.json().then(error => {
+          // then throw it
+          throw error;
+        });
+      }
+    })
+    .then(() => {
+      cb(noteId);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
 export default class NotePage extends Component {
   static contextType = NotefulContext;
 
@@ -15,17 +39,30 @@ export default class NotePage extends Component {
     }
 
     return (
-      <div className='note-content'>
-        <div className='note-header'>
-          <h2>{note.note_name}</h2>
-          {note.modified ? <p>Date modified on {note.modified}</p>
-                         : <p>(added by user)</p>
-          }
-        </div>
-        <div className='note-desc'>
-          <p>{note.content}</p>
-        </div>
-      </div>
+      <NotefulContext.Consumer>
+        {(context) => (
+          <div className='note-content'>
+            <div className='note-header'>
+              <h2>{note.note_name}</h2>
+              {note.modified ? <p>Date modified on {note.modified}</p>
+                            : <p>(added by user)</p>
+              }
+            </div>
+            <div className='note-desc'>
+              <p>{note.content}</p>
+            </div>
+            <button
+                className="delete-button"
+                onClick={() => {
+                  deleteNoteRequest(note.id, context.deleteNote)
+                  this.props.history.push('/')
+                }}
+              >
+                Delete
+            </button>
+          </div>
+        )}
+      </NotefulContext.Consumer>
     )
   }
 }
